f14f02c21bdf5b7b7fc177e960a9b2f646671719
Paweł Szmergała <pawelszme@gmail.com>
2021-09-14 21:00:24 +0200

    #1: Set up resizable fields on the board

--- src/main/java/com/example/solitaire/SolitaireController.java
+++ src/main/java/com/example/solitaire/SolitaireController.java
@@ -1,12 +1,27 @@
 package com.example.solitaire;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Rectangle;
 
 public class SolitaireController {
     @FXML
     private ToggleGroup boardType;
     @FXML
     private Label statusLabel;
+    @FXML
+    private GridPane board;
+
+    @FXML
+    void initialize() {
+        // bind fields' sizes to grid's size
+        for (Node node : board.getChildren()) {
+            Rectangle rect = (Rectangle) node;
+            rect.widthProperty().bind(board.widthProperty().divide(board.getRowCount()));
+            rect.heightProperty().bind(board.heightProperty().divide(board.getColumnCount()));
+        }
+    }
 }
