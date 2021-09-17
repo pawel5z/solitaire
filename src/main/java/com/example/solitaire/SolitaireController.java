ee4d8a08835b7b135eeac1086b5692b385798fcd
Paweł Szmergała <pawelszme@gmail.com>
2021-09-17 23:53:34 +0200

    #1: Implement peg moving with mouse

--- src/main/java/com/example/solitaire/SolitaireController.java
+++ src/main/java/com/example/solitaire/SolitaireController.java
@@ -4,10 +4,12 @@ import com.example.solitaire.backend.Solitaire;
 import javafx.beans.binding.Bindings;
 import javafx.beans.value.ObservableDoubleValue;
 import javafx.beans.value.ObservableIntegerValue;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
@@ -16,6 +18,7 @@ import javafx.util.Pair;
 
 public class SolitaireController {
     private Solitaire solitaire;
+    private Circle markedPeg = null;
 
     @FXML
     private ToggleGroup boardType;
@@ -36,6 +39,24 @@ public class SolitaireController {
         setPegs();
     }
 
+    @FXML
+    void fieldClicked(MouseEvent event) {
+        event.consume();
+        if (markedPeg == null) {
+            return;
+        }
+        Rectangle clickedField = (Rectangle) event.getTarget();
+        Pair<Integer, Integer> posToRemove = solitaire.move(GridPane.getRowIndex(markedPeg),
+                                                            GridPane.getColumnIndex(markedPeg),
+                                                            GridPane.getRowIndex(clickedField),
+                                                            GridPane.getColumnIndex(clickedField));
+        if (posToRemove == null)
+            return;
+        board.getChildren().remove(markedPeg);
+        board.add(markedPeg, GridPane.getColumnIndex(clickedField), GridPane.getRowIndex(clickedField));
+        board.getChildren().remove(getPegByRowCol(posToRemove.getKey(), posToRemove.getValue()));
+    }
+
     private void setPegs() {
         // remove any remaining pegs
         for (var peg : board.getChildren()) {
@@ -47,7 +68,7 @@ public class SolitaireController {
         solitaire = new Solitaire();
         for (Pair<Integer, Integer> pPos : solitaire.getPegPositions()) {
             Circle p = createPeg();
-            board.add(p, pPos.getKey(), pPos.getValue());
+            board.add(p, pPos.getValue(), pPos.getKey());
         }
     }
 
@@ -57,6 +78,19 @@ public class SolitaireController {
                 board.heightProperty().divide(board.getRowCount())).divide(2).multiply(0.9));
         peg.setFill(Paint.valueOf("0x000000"));
 
+        peg.setOnMouseClicked(mouseEvent -> {
+            mouseEvent.consume();
+            markedPeg = (Circle) mouseEvent.getTarget();
+        });
+
         return peg;
+    }
+
+    private Circle getPegByRowCol(int r, int c) {
+        for (var peg : board.getChildren()) {
+            if (peg instanceof Circle && GridPane.getRowIndex(peg) == r && GridPane.getColumnIndex(peg) == c)
+                return (Circle) peg;
+        }
+        return null;
     }
 }
